eb81e0d1e16fd6ce76b4cf36a0fbe19664766cd5
Harry Nystad <harnys@Server.(none)>
2013-04-27 16:31:49 +0200

    merging

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ try {
 
 	$temp = file_get_contents("pw.txt");
 	$pw = explode(" ",$temp);
-	$db = new PDO('mysql:host=95.34.97.5;dbname='."$pw[0]", "$pw[0]","$pw[1]");
+	$db = new PDO('mysql:host=harnys.net;dbname='."$pw[0]", "$pw[0]","$pw[1]");
 	} catch (PDOException $e) 
 	{
     	die ('Kunne ikke koble til serveren : ' . $e->getMessage());
