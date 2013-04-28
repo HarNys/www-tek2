e9062b961445db32433aa02e120d92ac2c49c773
Harry Nystad <harryjnystad@gmail.com>
2013-04-28 18:08:06 +0200

    fixed bug where if you havent commented on a project it would not show corecly

--- pages/projectsOverview.php
+++ pages/projectsOverview.php
@@ -9,11 +9,10 @@
 	// Connect to the database
 	require_once ("../db.php");
 
-	$sql = ' SELECT title, owner, shortTitle, projects.id , companyname, comment 
-			FROM projects  LEFT JOIN externalusers ON projects.owner = externalusers.id 
-			LEFT JOIN staffcomments ON staffcomments.projectid = projects.id WHERE uid LIKE ?';
+	$sql = ' SELECT title, owner, shortTitle, projects.id , companyname 
+			FROM projects  LEFT JOIN externalusers ON projects.owner = externalusers.id ';
 	$sth = $db->prepare ($sql);
-	$sth->execute(array($_SESSION['uid']));
+	$sth->execute();
 	
 	if ( $sth->rowCount() == 0 )
 	{
@@ -25,12 +24,23 @@
 
 		foreach( $result as $row )
 		{
+			
+			$sql = ' SELECT comment FROM  staffcomments LEFT JOIN projects ON staffcomments.projectid = projects.id WHERE uid LIKE ?';
+		
+			$stmt = $db->prepare ($sql);
+			$stmt->execute(array($_SESSION['uid']));
+			$comments = $stmt->fetch();
+			$stmt -> closeCursor();
+
+			echo " hahaha  ". $comments['comment'];
+
 			echo "Oppdragsgiver: " . $row['companyname'];
 			echo "<br />";
 			echo "<a class='showProjectInfo' id='" . $row['id'] . "' href = ''>" . $row['title'] . "</a>";
-			if($row['comment'] != "")
+			
+			if($comments['comment'] != "")
 			{
-				echo "<br /> <font color = 'green'><b>Du komenterte: </font></b>".$row['comment']." <br />";
+				echo "<br /> <font color = 'green'><b>Du komenterte: </font></b>".$comments['comment']." <br />";
 			}
 			else
 			{
